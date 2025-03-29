5c2ac4f8ebd851adbe6a6640cc564fabbbf21923
Ghost <rildiarov@gmail.com>
2025-03-29 12:42:14 +0400

    add view image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { RegisterComponent } from './component/register/register.component';
 import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: 'AV Run - Главная' },
+    { path: '', component: HomeComponent, title: 'All Run - Главная' },
     // { path: 'about-trainer', component: TrainerComponent, title: 'О тренере' },
     // { path: 'about-club', component: ClubComponent, title: 'О клубе' },
     { path: 'register', component: RegisterComponent, title: 'Регистрация' },
