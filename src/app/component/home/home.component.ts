13bd4292ce3e8666b6239b3dd08ba920c39b0489
Ghost <rildiarov@gmail.com>
2025-08-11 22:57:33 +0400

    add photos

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -13,6 +13,14 @@ import { WorkoutListComponent } from "../view/workout-list/workout-list.componen
 export class HomeComponent implements OnInit {
   constructor(private router: Router) {}
 
+  galleryImages = [
+    { url: "assets/images/command_people.jpg", alt: "Группа спортсменов" },
+    { url: "assets/images/photo_medal_woman.jpg", alt: "Женщина с медалью" },
+    { url: "assets/images/time_to_life_man.jpg", alt: "время жить!" },
+    { url: "assets/images/woman_man_photo.jpg", alt: "Мужчина и женщина" },
+    { url: "assets/images/woman_medal_woman.jpg", alt: "женщина с медалью" },
+  ]
+
   ngOnInit() {
   }
 
